924bcfe79f59be003e551b731373022e26178646
Nick Setters <njs154@uakron.edu>
2025-11-23 18:59:16 -0500

    Make database-dependent pages dynamic

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
-
+export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: 'Edit Invoice',
 };
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Metadata } from 'next';
-
+export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: 'Dashboard',
 };
